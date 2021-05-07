96096ce46bc4c1797582b867af8f5410a00cddad
jordanonuma <jordanonuma@outlook.com>
2021-05-06 17:00:50 -1000

    Defined Zombie[] struct with name and DNA properties.

--- Contract.sol
+++ Contract.sol
@@ -4,5 +4,9 @@ contract ZombieFactory {
 
     uint dnaDigits = 16;
     uint dnaModulus = 10**dnaDigits;
-    
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }//end struct{}
 } //end contract ZombieFactory {}
