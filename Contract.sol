76e3063ab393f3d9b7133bacb2932c0ccd79edfa
jordanonuma <jordanonuma@outlook.com>
2021-05-09 10:47:49 -1000

    User's specified name and generated DNA will be input to the zombies[] struct.

--- Contract.sol
+++ Contract.sol
@@ -13,6 +13,6 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function createZombie(string memory _name, uint _dna) public {
-        
+        zombies.push(Zombie(_name, _dna));
     } //end function createZombie()
 } //end contract ZombieFactory {}
