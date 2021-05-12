ae484da4842e25b50085f71e9b83e040ade898db
jordanonuma <jordanonuma@outlook.com>
2021-05-11 19:02:39 -1000

    Added frame of JS function that will return an unsigned integer representing pseudo random DNA.

--- Contract.sol
+++ Contract.sol
@@ -15,4 +15,8 @@ contract ZombieFactory {
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     } //end function createZombie()
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+
+    } //end function _generateRandomDna()
 } //end contract ZombieFactory {}
