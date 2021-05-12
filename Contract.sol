37e3fc4aef0b401d9a7cd3b9ad655485b84f9e6a
jordanonuma <jordanonuma@outlook.com>
2021-05-12 06:33:00 -1000

    Returns the modulus of the hash to produce 16-digit DNA integer.

--- Contract.sol
+++ Contract.sol
@@ -18,5 +18,6 @@ contract ZombieFactory {
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
         uint rand = keccak256(abi.encodePacked(_str));
+        return rand % dnaModulus;
     } //end function _generateRandomDna()
 } //end contract ZombieFactory {}
