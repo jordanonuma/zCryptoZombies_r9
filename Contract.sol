56cb7a2c84a6f3d097e9901ed16043646e48eae6
jordanonuma <jordanonuma@outlook.com>
2021-05-14 21:36:29 -1000

    Defined sequential zombie ID and used it to fire front end notification.

--- Contract.sol
+++ Contract.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 contract ZombieFactory {
 
     event NewZombie(uint zombieId, string name, uint dna);
-    
+
     uint dnaDigits = 16;
     uint dnaModulus = 10**dnaDigits;
 
@@ -16,6 +16,8 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push() - 1;
+        emit NewZombie(id, _name, _dna);
     } //end function createZombie()
 
     function _generateRandomDna(string memory _str) private view returns (uint) {
