517adbdaee7c7e480b2fe0412a9fbd4b37f86046
jordanonuma <jordanonuma@outlook.com>
2021-05-14 21:38:43 -1000

    Pushed the zombies[] array using Zombie[] struct.

--- Contract.sol
+++ Contract.sol
@@ -16,7 +16,7 @@ contract ZombieFactory {
 
     function _createZombie(string memory _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
-        uint id = zombies.push() - 1;
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
     } //end function createZombie()
 
