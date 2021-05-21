6c60a105ac09231830d28507b19cf044834e912f
jordanonuma <jordanonuma@outlook.com>
2021-05-20 20:22:04 -1000

    Added function require user own the zombie before attaching to myZombie[] struct.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -2,5 +2,9 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiefactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
-    
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        Zombie storage myZombie = zombies[_zombieId];
+    } //end function feedAndMultiply()
 } //end contract ZombieFeeding{}
