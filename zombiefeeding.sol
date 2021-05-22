a8386e2b0c5e295b9360a39ce9ba499a13fadbad
jordanonuma <jordanonuma@outlook.com>
2021-05-21 19:55:17 -1000

    Truncates the attackee's DNA integer and takes average.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -6,5 +6,8 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna)/2;
     } //end function feedAndMultiply()
 } //end contract ZombieFeeding{}
