27549fe4c5fd10968390e8953731c3d42a344d1f
jordanonuma <jordanonuma@outlook.com>
2021-05-23 10:03:29 -1000

    Defined function the interface will call.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -4,9 +4,20 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
     
     contract KittyInterface {
-    
+        function getKitty(uint256 _id) external view returns (
+            bool isGestating,
+            bool isReady,
+            uint256 cooldownIndex,
+            uint256 nextActionAt,
+            uint256 siringWithId,
+            uint256 birthTime,
+            uint256 matronId,
+            uint256 sireId,
+            uint256 generation,
+            uint256 genes
+        );
     } //end interface KittyInterface{}
-    
+
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
