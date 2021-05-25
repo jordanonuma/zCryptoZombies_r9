f24a9ca3aa0297ede15728d9237a0d0c75038a6d
jordanonuma <jordanonuma@outlook.com>
2021-05-24 17:38:09 -1000

    Corrected syntax.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,7 +19,7 @@ contract ZombieFeeding is ZombieFactory {
     } //end interface KittyInterface{}
 
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-    KittyInterface kittyContract = KittyInterface('ckAddress');
+    KittyInterface kittyContract = KittyInterface(ckAddress);
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
