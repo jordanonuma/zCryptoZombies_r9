00c3ffabb11b72607494e20e831b2f57d7bd2bda
jordanonuma <jordanonuma@outlook.com>
2021-05-25 18:29:44 -1000

    Corrected syntax for how DNA is stored.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -32,7 +32,7 @@ contract ZombieFeeding is ZombieFactory {
 
     function feedOnKitty(uint _zombieId, uint _kittyId) public {
         uint kittyDna;
-        kittyDna = kittyContract.getKitty(,,,,,,,,,_kittyId);
-        feedAndMutiply(_zombieId, kittyDna);
+        (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
     } //end function feedOnKitty()
 } //end contract ZombieFeeding{}
