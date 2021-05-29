87ec42893e3d9e351824e30514e082ca4b80e92c
jordanonuma <jordanonuma@outlook.com>
2021-05-29 11:33:17 -1000

    Added onlyOwner modifier to run from the Ownable{} contract.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -21,7 +21,7 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
     
     KittyInterface kittyContract;
-    function setKittyContractAddress(address _address) external {
+    function setKittyContractAddress(address _address) external onlyOwner {
         kittyContract = KittyInterface(_address);
     } //end function setKittyContractAddress()
 
