ca05f85a8f22cc07e3f50a62af939fc8d345e792
jordanonuma <jordanonuma@outlook.com>
2021-06-01 20:57:00 -1000

    Added frame of function that starts clock between attacks.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -25,6 +25,10 @@ contract ZombieFeeding is ZombieFactory {
         kittyContract = KittyInterface(_address);
     } //end function setKittyContractAddress()
 
+    function _triggerCooldown(Zombie storage _zombie) internal {
+    
+    } //end function _triggerCooldown()
+
     function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
