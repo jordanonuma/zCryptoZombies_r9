a51519e645494ff3c4825a9dbdde731cb915ff81
jordanonuma <jordanonuma@outlook.com>
2021-06-03 07:16:12 -1000

    Added requirement before command to return to main function.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -5,7 +5,8 @@ import "./zombiefeeding.sol";
 contract ZombieHelper is ZombieFeeding {
 
     modifier aboveLevel(uint _level, uint _zombieId) {
-
+        require(zombies[_zombieId].level >= _level);
+        _;
     } //end modifier aboveLevel()
 
 } //end contract ZombieHelper{}
