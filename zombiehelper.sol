506b3ec0d2a937dd3cf13ff2dfb8f566c8788100
jordanonuma <jordanonuma@outlook.com>
2021-06-04 08:03:51 -1000

    Revised logic and syntax.

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -10,11 +10,13 @@ contract ZombieHelper is ZombieFeeding {
     } //end modifier aboveLevel()
 
     function changeName (uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) {
-        require(msg.sender == zombies[_zombieId].name = _newName);
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
     } //end function _hangeName()
 
     function changeDna (uint _zombieId, uint  _newDna) external aboveLevel(20, _zombieId) {
-        require(msg.sender == zombies[_zombieId].dna = _newDna);
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
     } //end function changeDna()
 
 } //end contract ZombieHelper{}
