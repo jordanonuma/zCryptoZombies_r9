bf7b25d9b849e096a7d2e42301bdcbec63a9d9f4
jordanonuma <jordanonuma@outlook.com>
2021-06-13 09:08:30 -1000

    Uses modifier to make sure user owns zombie.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -37,8 +37,7 @@ contract ZombieFeeding is ZombieFactory {
         return (_zombie.readyTime <= now);
     } //end function _isReady()
 
-    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal {
-        require(msg.sender == zombieToOwner[_zombieId]);
+    function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) internal ownerOf(_zombieId) {
         Zombie storage myZombie = zombies[_zombieId];
         require(_isReady(myZombie));
 
