69dea5749791bea1525fa4384b5a2cebd143f20b
jordanonuma <jordanonuma@outlook.com>
2021-06-14 07:39:48 -1000

    Removed redundant require().

--- zombiehelper.sol
+++ zombiehelper.sol
@@ -26,12 +26,10 @@ contract ZombieHelper is ZombieFeeding {
     } //end function levelUp()
 
     function changeName (uint _zombieId, string calldata _newName) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].name = _newName;
     } //end function _hangeName()
 
     function changeDna (uint _zombieId, uint  _newDna) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
-        require(msg.sender == zombieToOwner[_zombieId]);
         zombies[_zombieId].dna = _newDna;
     } //end function changeDna()
 
