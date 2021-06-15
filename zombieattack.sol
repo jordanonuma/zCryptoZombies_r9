11fa37bb116fd2063445a8d74d3db9bb48c3239d
jordanonuma <jordanonuma@outlook.com>
2021-06-15 07:55:11 -1000

    Declare user and enemy zombie structs. Pseudo-random integer defined.

--- zombieattack.sol
+++ zombieattack.sol
@@ -11,6 +11,8 @@ contract ZombieAttack is ZombieHelper {
     } //end function randMod()
     
     function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
-    
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint rand = randMod(100);
     } //end function attack()
 } //end contract ZombieAttack{}
