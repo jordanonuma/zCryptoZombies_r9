5742f4ff86cd7c28ddb751115c503fa7bfa7b247
jordanonuma <jordanonuma@outlook.com>
2021-06-20 09:53:00 -1000

    Added frames of functions that user will call to return balances, zombie's owner, and when transferring zombie ownership.

--- zombieownership.sol
+++ zombieownership.sol
@@ -4,4 +4,19 @@ import  "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
+    function balanceOf(address _owner) external view returns (uint256) {
+        
+    } //end function balanceOf()
+
+    function ownerOf(uint256 _tokenId) external view returns (address) {
+        
+    } //end function ownerOf()
+
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+        
+    } //end function transferFrom()
+
+    function approve(address _approved, uint256 _tokenId) external payable {
+
+    } //end function approve()
 } //end contract ZombieOwnership{}
