a54e15098976fc46140c7fd2bf50c1e2ce3f64b4
jordanonuma <jordanonuma@outlook.com>
2021-06-27 09:22:55 -1000

    Replaced with SafeMath addition.

--- zombieownership.sol
+++ zombieownership.sol
@@ -15,7 +15,7 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     } //end function ownerOf()
 
     function _transfer(address _from, address _to, uint256 _tokenId) private {
-        ownerZombieCount[_to]++;
+        ownerZombieCount[_to] = ownerZombieCount[_to].add(1);
         ownerZombieCount[_from]--;
         zombieToOwner[_tokenId] = _to;
         emit Transfer(_from, _to, _tokenId);
