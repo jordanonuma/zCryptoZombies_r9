c5882b9cec21cd071011aa37bedece471d8aaced
jordanonuma <jordanonuma@outlook.com>
2021-07-10 10:34:19 -1000

    Added frame of Solidity function that fires before the match.

--- examplegame.sol
+++ examplegame.sol
@@ -4,6 +4,8 @@ import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
 
-
+    function beforeMatchStart(bytes serializedGameState) external {
+        
+    } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
