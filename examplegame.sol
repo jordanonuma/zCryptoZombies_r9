e072d63e56e89f31f9bc11696b73e8c6b435caf5
jordanonuma <jordanonuma@outlook.com>
2021-07-11 10:07:45 -1000

    Initialized gameState to read the data with serialized bytes.

--- examplegame.sol
+++ examplegame.sol
@@ -6,6 +6,7 @@ contract ExampleGame is ZBGameMode  {
 
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
+        gameState.init(serializedGameState);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
