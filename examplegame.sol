63001f269b06bf80f3ac0eb01a04ccad7f5c8b1b
jordanonuma <jordanonuma@outlook.com>
2021-07-11 10:06:20 -1000

    Declared gameState in memory.

--- examplegame.sol
+++ examplegame.sol
@@ -5,7 +5,7 @@ import "./ZB/ZBGameMode.sol";
 contract ExampleGame is ZBGameMode  {
 
     function beforeMatchStart(bytes serializedGameState) external {
-        
+        GameState memory gameState;
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
