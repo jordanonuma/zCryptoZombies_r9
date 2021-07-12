59cb922ba4b506ab1182b12be32751724929a091
jordanonuma <jordanonuma@outlook.com>
2021-07-12 05:42:21 -1000

    Added tracker for all the changes to serialized game state.

--- examplegame.sol
+++ examplegame.sol
@@ -7,6 +7,8 @@ contract ExampleGame is ZBGameMode  {
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
         gameState.init(serializedGameState);
+
+        ZBSerializer.SerializedGameStateChanges memory changes;
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
