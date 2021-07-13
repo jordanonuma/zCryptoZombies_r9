708b6786e811842920be64e39ff70e61cee0b055
jordanonuma <jordanonuma@outlook.com>
2021-07-13 06:20:46 -1000

    Bumped the max to 8, higher than the set goo-filled amount.

--- examplegame.sol
+++ examplegame.sol
@@ -20,8 +20,8 @@ contract ExampleGame is ZBGameMode  {
         changes.changePlayerCurrentGoo(Player.Player1, 3);
         changes.changePlayerCurrentGoo(Player.Player2, 3);
 
-        changes.changePlayerMaxGooVials(Player.Player1, 3);
-        changes.changePlayerMaxGooVials(Player.Player2, 3);
+        changes.changePlayerMaxGooVials(Player.Player1, 8);
+        changes.changePlayerMaxGooVials(Player.Player2, 8);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
