30427ac31809d335b48b5f58383291abfc3c0d13
jordanonuma <jordanonuma@outlook.com>
2021-07-13 06:17:11 -1000

    Sets player 1 and 2's defense to 15.

--- examplegame.sol
+++ examplegame.sol
@@ -10,6 +10,9 @@ contract ExampleGame is ZBGameMode  {
 
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
+
+        changes.changePlayerDefense(Player.Player1, 15);
+        changes.changePlayerDefense(Player.Player2, 15);
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
