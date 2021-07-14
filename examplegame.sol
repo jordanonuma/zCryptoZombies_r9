831758d63ad7fb4e50e0552eed8b36880d4932d0
jordanonuma <jordanonuma@outlook.com>
2021-07-14 07:50:47 -1000

    Declared mapping for banned cards.

--- examplegame.sol
+++ examplegame.sol
@@ -3,6 +3,7 @@ pragma solidity 0.4.25;
 import "./ZB/ZBGameMode.sol";
 
 contract ExampleGame is ZBGameMode  {
+    mapping (string => bool) internal bannedCards;
 
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
