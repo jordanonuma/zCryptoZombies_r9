3a6f1f944e611e26c9a627ff0736aa52ddd59b5d
jordanonuma <jordanonuma@outlook.com>
2021-07-14 07:51:36 -1000

    Declared list of banned cards.

--- examplegame.sol
+++ examplegame.sol
@@ -5,6 +5,32 @@ import "./ZB/ZBGameMode.sol";
 contract ExampleGame is ZBGameMode  {
     mapping (string => bool) internal bannedCards;
 
+    constructor() public {
+        bannedCards["Leash"] = true;
+        bannedCards["Bulldozer"] = true;
+        bannedCards["Lawnmower"] = true;
+        bannedCards["Shopping Cart"] = true;
+        bannedCards["Stapler"] = true;
+        bannedCards["Nail Bomb"] = true;
+        bannedCards["Goo Bottles"] = true;
+        bannedCards["Molotov"] = true;
+        bannedCards["Super Goo Serum"] = true;
+        bannedCards["Junk Spear"] = true;
+        bannedCards["Fire Extinguisher"] = true;
+        bannedCards["Fresh Meat"] = true;
+        bannedCards["Chainsaw"] = true;
+        bannedCards["Bat"] = true;
+        bannedCards["Whistle"] = true;
+        bannedCards["Supply Drop"] = true;
+        bannedCards["Goo Beaker"] = true;
+        bannedCards["Zed Kit"] = true;
+        bannedCards["Torch"] = true;
+        bannedCards["Shovel"] = true;
+        bannedCards["Boomstick"] = true;
+        bannedCards["Tainted Goo"] = true;
+        bannedCards["Corrupted Goo"] = true;
+    } //end constructor()
+
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
         gameState.init(serializedGameState);
