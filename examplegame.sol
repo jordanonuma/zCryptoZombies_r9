a59226c7bcba23cbe46be84992521a2d1850fe94
jordanonuma <jordanonuma@outlook.com>
2021-07-15 07:54:17 -1000

    Added for() loop that goes through both players' cards.

--- examplegame.sol
+++ examplegame.sol
@@ -49,6 +49,10 @@ contract ExampleGame is ZBGameMode  {
 
         changes.changePlayerMaxGooVials(Player.Player1, 8);
         changes.changePlayerMaxGooVials(Player.Player2, 8);
+
+        for (uint i = 0; i < gameState.playerStates.length; i++) {
+            
+        } //end for()
     } //end function beforeMatchStart()
 
 } //end contract ExampleGame{}
