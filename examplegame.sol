267d1410b98829d364863f05c061035aaf5a9975
jordanonuma <jordanonuma@outlook.com>
2021-07-16 06:26:12 -1000

    Checks if the card is allowed to be played. Allowable cards are then passed to newCards[] array and the card counter is incremented.

--- examplegame.sol
+++ examplegame.sol
@@ -54,7 +54,10 @@ contract ExampleGame is ZBGameMode  {
             CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
             uint cardCount = 0;
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                
+                if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                } //end if()
             } //end for(j)
         } //end for(i)
     } //end function beforeMatchStart()
