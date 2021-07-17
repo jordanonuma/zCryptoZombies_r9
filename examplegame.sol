f2a76b3aa45ef9830f88201f0b0b31fc2556451b
jordanonuma <jordanonuma@outlook.com>
2021-07-17 11:36:25 -1000

    Revised order for syntax.

--- examplegame.sol
+++ examplegame.sol
@@ -62,7 +62,7 @@ contract ExampleGame is ZBGameMode  {
         } //end for(i)
     } //end function beforeMatchStart()
 
-    function isLegalCard(CardInstance card) returns (bool) internal view {
+    function isLegalCard(CardInstance card) internal view returns (bool) {
         return (!bannedCards[card.mouldName]);
     } //end function isLegalCard()
 
