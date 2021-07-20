19bfe325d64cd9d7ea3c107e877ca7e0770cd6fa
jordanonuma <jordanonuma@outlook.com>
2021-07-20 07:28:37 -1000

    Removed game logic that won't be applicable.

--- munchkin.sol
+++ munchkin.sol
@@ -5,32 +5,6 @@ import "./ZB/ZBGameMode.sol";
 contract Munchkin is ZBGameMode  {
     mapping (string => bool) internal bannedCards;
 
-    constructor() public {
-        bannedCards["Leash"] = true;
-        bannedCards["Bulldozer"] = true;
-        bannedCards["Lawnmower"] = true;
-        bannedCards["Shopping Cart"] = true;
-        bannedCards["Stapler"] = true;
-        bannedCards["Nail Bomb"] = true;
-        bannedCards["Goo Bottles"] = true;
-        bannedCards["Molotov"] = true;
-        bannedCards["Super Goo Serum"] = true;
-        bannedCards["Junk Spear"] = true;
-        bannedCards["Fire Extinguisher"] = true;
-        bannedCards["Fresh Meat"] = true;
-        bannedCards["Chainsaw"] = true;
-        bannedCards["Bat"] = true;
-        bannedCards["Whistle"] = true;
-        bannedCards["Supply Drop"] = true;
-        bannedCards["Goo Beaker"] = true;
-        bannedCards["Zed Kit"] = true;
-        bannedCards["Torch"] = true;
-        bannedCards["Shovel"] = true;
-        bannedCards["Boomstick"] = true;
-        bannedCards["Tainted Goo"] = true;
-        bannedCards["Corrupted Goo"] = true;
-    } //end constructor()
-
     function beforeMatchStart(bytes serializedGameState) external {
         GameState memory gameState;
         gameState.init(serializedGameState);
@@ -38,35 +12,7 @@ contract Munchkin is ZBGameMode  {
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
 
-        changes.changePlayerDefense(Player.Player1, 15);
-        changes.changePlayerDefense(Player.Player2, 15);
-
-        changes.changePlayerCurrentGooVials(Player.Player1, 3);
-        changes.changePlayerCurrentGooVials(Player.Player2, 3);
-
-        changes.changePlayerCurrentGoo(Player.Player1, 3);
-        changes.changePlayerCurrentGoo(Player.Player2, 3);
-
-        changes.changePlayerMaxGooVials(Player.Player1, 8);
-        changes.changePlayerMaxGooVials(Player.Player2, 8);
-
-        for (uint i = 0; i < gameState.playerStates.length; i++) {
-            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
-            uint cardCount = 0;
-            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                if(isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
-                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
-                    cardCount++;
-                } //end if()
-            } //end for(j)
-            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
-        } //end for(i)
-
         changes.emit();
     } //end function beforeMatchStart()
-
-    function isLegalCard(CardInstance card) internal view returns (bool) {
-        return (!bannedCards[card.mouldName]);
-    } //end function isLegalCard()
 
 } //end contract Munchkin{}
