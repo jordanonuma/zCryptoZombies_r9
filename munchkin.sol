c31841cfe5ba20d9b4af6405a4c9092ee6e6f5cd
jordanonuma <jordanonuma@outlook.com>
2021-07-21 06:39:04 -1000

    For() loops will iterate through each player's cards.

--- munchkin.sol
+++ munchkin.sol
@@ -12,6 +12,20 @@ contract Munchkin is ZBGameMode  {
         ZBSerializer.SerializedGameStateChanges memory changes;
         changes.init();
 
+        for (uint i = 0; i < gameState.playerStates.length; i++) {
+            CardInstance[] memory newCards = new CardInstance[](gameState.playerStates[i].cardsInDeck.length);
+            uint cardCount = 0;
+
+            for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
+                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
+                    cardCount++;
+                } //end if()
+            } //end for(j)
+
+            changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
+        } //end for(i)
+
         changes.emit();
     } //end function beforeMatchStart()
 
