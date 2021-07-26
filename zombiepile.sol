28fc8b1717080a51b910976e9b0831c0a7059c60
jordanonuma <jordanonuma@outlook.com>
2021-07-26 05:45:40 -1000

    Takes the modulus of a pseudo-random number.

--- zombiepile.sol
+++ zombiepile.sol
@@ -20,18 +20,7 @@ contract ZombiePile is ZBGameMode  {
         for (uint i = 0; i < gameState.playerStates.length; i++) {
             
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                bool cardAlreadyInDeck == false;
-
-                for (uint k = 0; k < cardCount; k++) {
-                    if (keccak256(abi.encodePacked(newCards[k].mouldName)) == keccak256(abi.encodePacked(gameState.playerStates[i].cardsInDeck[j].mouldName))) {
-                        cardAlreadyInDeck = true;
-                    } //end if()
-                } //end for(k)
-
-                if (!cardAlreadyInDeck) {
-                    newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
-                    cardCount++;
-                } //end if()
+                uint rand = uint(keccak256(abi.encodePacked(now, player1CardCount + player2CardCount))) % 2;
             } //end for(j)
 
             changes.changePlayerCardsInDeck(Player(i), newCards, cardCount);
