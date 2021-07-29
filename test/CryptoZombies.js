9f831f1ee75f301d2382867602358c40f70814c0
jordanonuma <jordanonuma@outlook.com>
2021-07-29 07:24:05 -1000

    Sets up frame and description of test.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,1 +1,7 @@
 const CryptoZombies = artifacts.require("CryptoZombies.js");
+    
+contract("CryptoZombies.js", (accounts) => {
+    it("should be able to create a new zombie", () => {
+       
+    })
+})
