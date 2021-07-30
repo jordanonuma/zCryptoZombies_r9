6cfff34a3b8e188b9e3b1d5171071a5e1b51ffd9
jordanonuma <jordanonuma@outlook.com>
2021-07-30 07:30:42 -1000

    Prepended for asynchronous talks to the private chain.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -3,7 +3,7 @@ const CryptoZombies = artifacts.require("CryptoZombies");
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
 
-    it("should be able to create a new zombie", () => {
+    it("should be able to create a new zombie", async () => {
        
     }) //end it()
 }) //end contract{}
