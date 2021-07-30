6b1dcf67a125dd01cab8e1433a7fac35674324fc
jordanonuma <jordanonuma@outlook.com>
2021-07-30 07:29:30 -1000

    Initialized Alice's and Bob's account.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,7 +1,9 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
     
 contract("CryptoZombies", (accounts) => {
+    let [alice, bob] = accounts;
+
     it("should be able to create a new zombie", () => {
        
-    })
-})
+    }) //end it()
+}) //end contract{}
