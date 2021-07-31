f64bdef7f54645ebcd1440ca1d05aa499e7a7e28
jordanonuma <jordanonuma@outlook.com>
2021-07-31 09:27:46 -1000

    Created a JS object called 'contractInstance' to interact with smart contract.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -4,6 +4,6 @@ contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
 
     it("should be able to create a new zombie", async () => {
-       
+        const contractInstance = await CryptoZombies.new();
     }) //end it()
 }) //end contract{}
