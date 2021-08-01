9561db3466eff2f37e9241d8e0051ee68ce4a3a9
jordanonuma <jordanonuma@outlook.com>
2021-08-01 09:01:33 -1000

    Truffle will store transaction results in result.logs[].

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -6,5 +6,6 @@ contract("CryptoZombies", (accounts) => {
 
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
     }) //end it()
 }) //end contract{}
