5865da3ed5de3f82e5c5c5da20097555e1831bbf
jordanonuma <jordanonuma@outlook.com>
2021-08-01 09:02:17 -1000

    Checks if transaction has passed.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -7,5 +7,6 @@ contract("CryptoZombies", (accounts) => {
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
         const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        assert.equal(result.receipt.status, true);
     }) //end it()
 }) //end contract{}
