00ae8928f0fba1f8fbab2098194a67fd2dce86fb
jordanonuma <jordanonuma@outlook.com>
2021-08-03 07:14:31 -1000

    Sent second zombie name as parameter for the test.

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -16,7 +16,7 @@ contract("CryptoZombies", (accounts) => {
     }) //end it()
 
     it("should not allow two zombies", async () => {
-        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-        await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[0]));
+        await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+        await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
     }) //end it()
 }) //end contract{}
