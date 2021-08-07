0df804f8f04a50b02ac50c22ffcedde001a0c004
jordanonuma <jordanonuma@outlook.com>
2021-08-07 09:28:57 -1000

    Tests case where alice calls transferFrom().

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -43,7 +43,15 @@ contract("CryptoZombies", (accounts) => {
             assert.equal(newOwner,bob);
         }) //end it()
         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
-            // TODO: Test the two-step scenario.  The owner calls transferFrom
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            
+            //alice calls the approve() function
+            await contractInstance.approve(bob, zombieId, {from: alice});
+            //alice calls transferFrom() to withdraw
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
          })
     }) //end context()
 }) //end contract{}
