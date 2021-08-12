114ab3de29886604c053d76da6c215a6df4478b2
jordanonuma <jordanonuma@outlook.com>
2021-08-12 07:29:52 -1000

    Declared address variable for the oracle and added frame of function that will allow owner to edit.

--- EthPriceOracle/caller/CallerContract.sol
+++ EthPriceOracle/caller/CallerContract.sol
@@ -1,5 +1,8 @@
 pragma solidity 0.5.0;
 
 contract CallerContract {
-    
+    address private oracleAddress;
+    function setOracleInstanceAddress(address _oracleInstanceAddress) public {
+        
+    } //end function setOracleInstanceAddress()
 } //end CallerContract{}
