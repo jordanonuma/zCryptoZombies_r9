3ac5b0026e2fb5d9f4b458c18840881fd11a7ab6
jordanonuma <jordanonuma@outlook.com>
2021-08-12 07:30:56 -1000

    Sets the oracle address.

--- EthPriceOracle/caller/CallerContract.sol
+++ EthPriceOracle/caller/CallerContract.sol
@@ -3,6 +3,6 @@ pragma solidity 0.5.0;
 contract CallerContract {
     address private oracleAddress;
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
-        
+        oracleAddress = _oracleInstanceAddress;
     } //end function setOracleInstanceAddress()
 } //end CallerContract{}
