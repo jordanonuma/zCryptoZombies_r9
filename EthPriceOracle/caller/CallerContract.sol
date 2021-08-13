adb937e628e1548a1c923e60a663c459078737bc
jordanonuma <jordanonuma@outlook.com>
2021-08-13 07:32:34 -1000

    Instantiates the interface in the instance.

--- EthPriceOracle/caller/CallerContract.sol
+++ EthPriceOracle/caller/CallerContract.sol
@@ -2,9 +2,10 @@ pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
 
 contract CallerContract {
-    EthPriceOracleInterface private EthPriceOracle;
+    EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
+        oracleInstance = EthPriceOracleInterface(oracleAddress);
     } //end function setOracleInstanceAddress()
 } //end CallerContract{}
