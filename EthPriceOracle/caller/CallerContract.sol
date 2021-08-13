78cfba0b667c88af38cfd5f8d4ae91a9a68f8458
jordanonuma <jordanonuma@outlook.com>
2021-08-13 07:28:34 -1000

    Imported new helper and declared local variable of the interface type.

--- EthPriceOracle/caller/CallerContract.sol
+++ EthPriceOracle/caller/CallerContract.sol
@@ -1,6 +1,8 @@
 pragma solidity 0.5.0;
+import "./EthPriceOracleInterface.sol";
 
 contract CallerContract {
+    EthPriceOracleInterface private EthPriceOracle;
     address private oracleAddress;
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
