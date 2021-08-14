148e9ef3d2721d79ae7b9fec9ca957de65e8bb01
jordanonuma <jordanonuma@outlook.com>
2021-08-14 09:02:57 -1000

    Inherits Ownable contract. Declares new oracle address being made event.

--- EthPriceOracle/caller/CallerContract.sol
+++ EthPriceOracle/caller/CallerContract.sol
@@ -2,9 +2,11 @@ pragma solidity 0.5.0;
 import "./EthPriceOracleInterface.sol";
 import "../../openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
-contract CallerContract {
+contract CallerContract is Ownable {
     EthPriceOracleInterface private oracleInstance;
     address private oracleAddress;
+    event newOracleAddressEvent(address oracleAddress);
+    
     function setOracleInstanceAddress(address _oracleInstanceAddress) public {
         oracleAddress = _oracleInstanceAddress;
         oracleInstance = EthPriceOracleInterface(oracleAddress);
