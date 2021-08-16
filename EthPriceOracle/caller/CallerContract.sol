7f991eec0c270fa96499b1ac8e909bdfe2425cea
jordanonuma <jordanonuma@outlook.com>
2021-08-16 07:24:20 -1000

    Revised syntax and variable name.

--- EthPriceOracle/caller/CallerContract.sol
+++ EthPriceOracle/caller/CallerContract.sol
@@ -24,9 +24,9 @@ contract CallerContract is Ownable {
     } //end function updateEthPrice()
 
     function callback(uint256 _ethPrice, uint256 _id) public {
-        require(myRequests[id] == true, "This request is not in my pending list.");
+        require(myRequests[_id], "This request is not in my pending list.");
         ethPrice = _ethPrice;
-        delete myRequests[id];
-        emit PriceUpdatedEevent(_ethPrice, _id);
+        delete myRequests[_id];
+        emit PriceUpdatedEvent(_ethPrice, _id);
     } //end function callback()
 } //end CallerContract{}
