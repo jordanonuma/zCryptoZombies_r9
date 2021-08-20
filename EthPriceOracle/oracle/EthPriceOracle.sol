d0c8c34039395419793e5b16b78cd3ebab9edac8
jordanonuma <jordanonuma@outlook.com>
2021-08-20 08:41:27 -1000

    Checks for queue in pending requests then deletes the request in the mapping after processed.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -22,6 +22,7 @@ contract EthPriceOracle is Ownable {
     } //end function getLatestEthPrice()
 
     function setLatestEthPrice(uint256 _ethPrice, address callerAddress, uint256 id) public {
-      
+        require(pendingRequests[_id], "This request is not in my pending list.");
+        delete pendingRequests[_id];
     } //end function setLatestEthPrice()
 } //end contract EthPriceOracle{}
