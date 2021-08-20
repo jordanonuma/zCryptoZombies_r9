696f48708686cfe424181dace6567a815fa615e2
jordanonuma <jordanonuma@outlook.com>
2021-08-20 08:43:16 -1000

    Revised variables for syntax and added modifier so the owner is solely able to call the function.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -21,7 +21,7 @@ contract EthPriceOracle is Ownable {
         return id;
     } //end function getLatestEthPrice()
 
-    function setLatestEthPrice(uint256 _ethPrice, address callerAddress, uint256 id) public {
+    function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
         require(pendingRequests[_id], "This request is not in my pending list.");
         delete pendingRequests[_id];
     } //end function setLatestEthPrice()
