09a2c7bacbc469bd56085d3880349a2651fc53c7
jordanonuma <jordanonuma@outlook.com>
2021-08-22 08:18:38 -1000

    Added frame of async JS function. Sets network ID.

--- EthPriceOracle/EthPriceOracle.js
+++ EthPriceOracle/EthPriceOracle.js
@@ -7,3 +7,7 @@ const CHUNK_SIZE = process.env.CHUNK_SIZE || 3
 const MAX_RETRIES = process.env.MAX_RETRIES || 5
 const OracleJSON = require('./oracle/build/contracts/EthPriceOracle.json') //Just an example. Actual JSON not compiled to this directory tree.
 var pendingRequests = []
+
+async function getOracleContract(web3js) {
+    const networkId = await web3js.eth.net.getId;
+} //end function getOracleContract()
