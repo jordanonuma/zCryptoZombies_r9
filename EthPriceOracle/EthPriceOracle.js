b30cff0a40b37f874eea83592cb920079cbffe9e
jordanonuma <jordanonuma@outlook.com>
2021-08-26 18:46:43 -1000

    Revised syntax.

--- EthPriceOracle/EthPriceOracle.js
+++ EthPriceOracle/EthPriceOracle.js
@@ -41,7 +41,7 @@ async function processQueue (oracleContract, ownerAddress) {
     let processedRequests = 0
     while (pendingRequests.length >0 && processedRequests < CHUNK_SIZE) {
         const req = pendingRequests.shift()
-        await processRequest(oracleContract, ownerAddress, req.id, req.callerAddress);
+        await processRequest(oracleContract, ownerAddress, req.id, req.callerAddress)
         processedRequests++
     } //end while()
 } //end function processQueue()
