061d8be72b2851b4f25bc45b54cdd24ade8c1790
jordanonuma <jordanonuma@outlook.com>
2021-09-03 19:10:45 -1000

    Attaches the 'Roles' library to the Roles.Role data type. Declared the owner and oracle variables of Roles.Role data type.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -1,8 +1,13 @@
 pragma solidity 0.5.0;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol"; //file path may not be exact
+import "openzeppelin-solidity/contracts/access/Roles.sol"; //file path may not be exact
 import "./CallerContractInterface.sol"; //file path may not be exact
 
-contract EthPriceOracle is Ownable {
+contract EthPriceOracle {
+    using Roles for Roles.Role;
+    Roles.Role private owners;
+    Roles.Role private oracles;
+    
     uint private randNonce = 0;
     uint private modulus = 1000;
     mapping(uint256=>bool) pendingRequests;
