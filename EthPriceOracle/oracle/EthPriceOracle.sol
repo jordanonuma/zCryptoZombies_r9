ea5522425cc400232755000699a37eda03b52412
jordanonuma <jordanonuma@outlook.com>
2021-09-05 09:28:45 -1000

    Defined event where an oracle has been added to the approved list.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -13,6 +13,7 @@ contract EthPriceOracle {
     mapping(uint256=>bool) pendingRequests;
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+    event AddOracleEvent(address oracleAddress);
 
     constructor (address _owner) public {
         owners.add(_owner);
