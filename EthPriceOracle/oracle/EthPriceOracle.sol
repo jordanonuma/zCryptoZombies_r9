f9b0497df27cc4c8d74a8ed28df9f55bfd008f12
jordanonuma <jordanonuma@outlook.com>
2021-09-06 09:53:50 -1000

    Lets front end know an oracle has been added.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -23,6 +23,7 @@ contract EthPriceOracle {
         require(owners.has(msg.sender), "Not an owner!"); //Checks if user is an owner
         require(!oracles.has(_oracle), "Already an oracle!"); //Checks if oracle already on list
         oracle.add(_oracle);
+        emit AddOracleEvent(_oracle);
     } //end function addOracle()
     
     function getLatestEthPrice() public returns(uint256) {
