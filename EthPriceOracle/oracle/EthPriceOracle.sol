55daf557482a501faf840a8df2683181da89bab8
jordanonuma <jordanonuma@outlook.com>
2021-09-07 17:30:41 -1000

    Defined event when an oracle has been removed.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -11,11 +11,12 @@ contract EthPriceOracle {
     uint private randNonce = 0;
     uint private modulus = 1000;
     uint private numOracles = 0;
-    
+
     mapping(uint256=>bool) pendingRequests;
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
     event AddOracleEvent(address oracleAddress);
+    event RemoveOracleEvent(address oracleAddress);
 
     constructor (address _owner) public {
         owners.add(_owner);
