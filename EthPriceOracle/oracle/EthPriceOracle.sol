b2d7c3cd39e3872eb7eca61bf9e48cd9aeb4949c
jordanonuma <jordanonuma@outlook.com>
2021-09-07 17:30:01 -1000

    Initializes variable that tracks the number of oracles.

--- EthPriceOracle/oracle/EthPriceOracle.sol
+++ EthPriceOracle/oracle/EthPriceOracle.sol
@@ -10,6 +10,8 @@ contract EthPriceOracle {
 
     uint private randNonce = 0;
     uint private modulus = 1000;
+    uint private numOracles = 0;
+    
     mapping(uint256=>bool) pendingRequests;
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
