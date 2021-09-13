37d48b2af6f95fdc26693237a84f51afa0a6605b
jordanonuma <jordanonuma@outlook.com>
2021-09-13 06:00:58 -1000

    Defines zkSyncProvider within try-catch{}.

--- EthPriceOracle/oracle/utils.js
+++ EthPriceOracle/oracle/utils.js
@@ -1,4 +1,9 @@
 async function getZkySyncProvider (zksync, networkName) {
     let zkSyncProvider;
-    
+    try {
+        zkySyncProvider = await zksync.getDefaultProvider(networkName)
+    } catch () {
+        console.log('Unable to connecto to zkSync.');
+        console.log(error)
+    } //end try-catch{}
 } //end function getZkySyncProvider()
