fd5b1f098380a12bbd625b898af0be7d44c5a138
jordanonuma <jordanonuma@outlook.com>
2021-09-14 20:07:59 -1000

    Added frame of JS function that interfaces with provider.

--- EthPriceOracle/oracle/utils.js
+++ EthPriceOracle/oracle/utils.js
@@ -8,3 +8,15 @@ async function getZkSyncProvider (zksync, networkName) {
     } //end try-catch{}
     return zkSyncProvider
 } //end function getZkySyncProvider()
+
+async function getEthereumProvider (ethers, networkName) {
+    let ethersProvider
+    try {
+        // eslint-disable-next-line new-cap
+        ethersProvider = new ethers.getDefaultProvider(networkName)
+    } catch (error) {
+        console.log('Could not connect to Ethereum')
+        console.log(error)
+    } //end try-catch{}
+    return ethersProvider
+  } //end function getEthereumProvider()
