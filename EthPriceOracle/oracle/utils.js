3452c92bfeb48ffc0b291e71ed4495886d2a243d
jordanonuma <jordanonuma@outlook.com>
2021-09-15 17:42:50 -1000

    Checks if an account has been made on zkSync.

--- EthPriceOracle/oracle/utils.js
+++ EthPriceOracle/oracle/utils.js
@@ -29,6 +29,8 @@ async function initAccount (rinkebyWallet, zkSyncProvider, zksync) {
 async function registerAccount (wallet) {
     console.log(`Registering the ${wallet.address()} account on zkSync`)
     if (!await wallet.isSigningKeySet()) {
-      
+        if (await wallet.getAccountId() === undefined) {
+            throw new Error('Unknown account')
+        } //end if()
     } //end if()
 } //end function registerAccount()
