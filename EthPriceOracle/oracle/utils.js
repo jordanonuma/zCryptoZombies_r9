f0ffbc34075d08cfda3df3acc3cc300ab1c8b05a
jordanonuma <jordanonuma@outlook.com>
2021-09-18 09:12:01 -1000

    Added frame of JS function that retrieves zkSync fee.

--- EthPriceOracle/oracle/utils.js
+++ EthPriceOracle/oracle/utils.js
@@ -65,3 +65,7 @@ async function transfer (from, toAddress, amountToTransfer, transferFee, token, 
     console.log('Got transfer receipt.')
     console.log(transferReceipt)
 } //end function transfer()
+
+async function getFee (transactionType, address, token, zkSyncProvider, ethers) {
+    
+} //edn function getFee()
