d205a81fcf49140b6ceadce77afdf529b8af784a
jordanonuma <jordanonuma@outlook.com>
2021-09-21 06:29:07 -1000

    Declared providers.

--- bob.js
+++ bob.js
@@ -3,5 +3,6 @@
     const zksync = require('zksync')
     const utils = require('./utils')
   
-    
+    const zkSyncprovider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
+    const ethersProvider = await utils.getEthereumProvider(zksync, process.env.NETWORK_NAME)
   })()
