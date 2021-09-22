ee043be0b4a2fee92a09d3b764d305b3637e0fe7
jordanonuma <jordanonuma@outlook.com>
2021-09-22 06:12:53 -1000

    Declared new wallet for Bob. Displays his wallet address.

--- bob.js
+++ bob.js
@@ -5,4 +5,7 @@
   
     const zkSyncProvider = await utils.getZkSyncProvider(zksync, process.env.NETWORK_NAME)
     const ethersProvider = await utils.getEthereumProvider(ethers, process.env.NETWORK_NAME)
+
+    const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_Key, ethersProvider)
+    console.log(`Bob's Rinkeby address is: ${rinkebyWallet.address}`)
   })()
