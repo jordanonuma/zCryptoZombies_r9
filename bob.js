4a9208e8ce1e9962f622413712acc532d0893b97
jordanonuma <jordanonuma@outlook.com>
2021-09-23 06:39:21 -1000

    Calls the zk function to display balance.

--- bob.js
+++ bob.js
@@ -17,7 +17,7 @@
         process.exit()
     })
     setInterval(async () => {
-        // Call the `utils.displayZkSyncBalance` function
+        await utils.displayZkSyncBalance(bobZkSyncWallet, ethers)
         console.log('---')
     }, SLEEP_INTERVAL)
   })()
