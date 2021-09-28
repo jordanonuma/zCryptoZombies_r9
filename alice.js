14fbf5873342db939b3b6f10aef5dfa50e72ee3c
jordanonuma <jordanonuma@outlook.com>
2021-09-28 06:58:08 -1000

    Gets and displays Rinkeby balance.

--- alice.js
+++ alice.js
@@ -12,11 +12,13 @@
     console.log('Creating a new Rinkeby wallet for Alice')
     const aliceRinkebyWallet = new ethers.Wallet(process.env.ALICE_PRIVATE_KEY, ethersProvider) // Account #78
     console.log(`Alice's Rinkeby address is: ${aliceRinkebyWallet.address}`)
-    const aliceInitialRinkebyBalance = await aliceRinkebyWallet.getBalance()
-    console.log(`Alice's initial balance on Rinkeby is: ${ethers.utils.formatEther(aliceInitialRinkebyBalance)}`)
   
     console.log('Creating a zkSync wallet for Alice')
     const aliceZkSyncWallet = await utils.initAccount(aliceRinkebyWallet, zkSyncProvider, zksync)
+
+    const tokenSet = zkSyncProvider.tokenSet
+    const aliceInitialRinkebyBalance = await aliceZkSyncWallet.getEthereumBalance(token)
+    console.log(`Alice's initial balance on Rinkeby is: ${tokenSet.formatToken(token, aliceInitialRinkebyBalance)}`)
   
     console.log('Depositing')
     await utils.depositToZkSync(aliceZkSyncWallet, token, amountToDeposit, ethers) 
