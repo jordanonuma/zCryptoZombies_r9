b999ff474c7e9a0d89244bc96055b4f4bcb28bcf
jordanonuma <jordanonuma@outlook.com>
2021-10-01 18:22:17 -1000

    Updated to use the tokenSet object.

--- EthPriceOracle/oracle/utils.js
+++ EthPriceOracle/oracle/utils.js
@@ -38,7 +38,7 @@ async function registerAccount (wallet) {
     } //end if()
 } //end function registerAccount()
 
-async function depositToZkSync (zkSyncWallet, token, amountToDeposit, ethers) {
+async function depositToZkSync (zkSyncWallet, token, amountToDeposit, tokenSet) {
     const deposit = await zkSyncWallet.depositToSyncFromEthereum({
         depositTo: zkSyncWallet.address(),
         token: token,
