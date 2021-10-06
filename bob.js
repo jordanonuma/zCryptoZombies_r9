79ba6618a85d6afa87c9baa06b99003c11d6afb1
jordanonuma <jordanonuma@outlook.com>
2021-10-05 20:54:53 -1000

    Revised zkSync.

--- bob.js
+++ bob.js
@@ -8,8 +8,8 @@
 
     const bobRinkebyWallet = new ethers.Wallet(process.env.BOB_PRIVATE_KEY, ethersProvider)
     console.log(`Bob's Rinkeby address is: ${bobRinkebyWallet.address}`)
-    console.log(`Bob's initial balance on Rinkeby is: ${ethers.utils.formatEther(await bobRinkebyWallet.getBalance())}`)
     const bobZkSyncWallet = await utils.initAccount(bobRinkebyWallet, zkSyncProvider, zksync)
+    console.log(`Bob's initial balance on Rinkeby is: ${await bobZkSyncWallet.getEthereumBalance(token)}`)
 
     process.on('SIGINT', () => {
         console.log('Disconnecting')
@@ -17,7 +17,7 @@
         process.exit()
     })
     setInterval(async () => {
-        await utils.displayZkSyncBalance(bobZkSyncWallet, ethers)
+        await utils.displayZkSyncBalance(bobZkSyncWallet, tokenSet)
         console.log('---')
     }, SLEEP_INTERVAL)
   })()
